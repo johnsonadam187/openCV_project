49d52e6179965fc2e7922d76cab08a532788477b
Adam Johnson <johnsonadam187@gmail.com>
2020-04-15 19:57:31 +1200

    added first test imshow()

--- Open_cv/open_cv.py
+++ Open_cv/open_cv.py
@@ -1,4 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn
+import cv2
+
+img = cv2.imread(r"C:\Users\johns\Pictures\Saved Pictures\evanholy.jpg", cv2.IMREAD_GRAYSCALE)
+cv2.imshow("Holyfield vs Tyson", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
