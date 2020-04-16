b5dbfd2eaf046784382e7d8b030e69bc9a3ec9f4
Adam Johnson <johnsonadam187@gmail.com>
2020-04-16 21:44:22 +1200

    added first test imshow()

--- Open_cv/open_cv.py
+++ Open_cv/open_cv.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn
 import cv2
 
-img = cv2.imread(r"C:\Users\johns\Pictures\Saved Pictures\evanholy.jpg", cv2.IMREAD_GRAYSCALE)
-cv2.rectangle(img, (10,10), (150, 150), (255, 255, 0), 5)
-cv2.circle(img, (75, 75), 75, (255, 0, 255), 5)
+img = cv2.imread(r"C:\Users\johns\Pictures\Saved Pictures\evanholy.jpg", cv2.IMREAD_COLOR)
+cv2.rectangle(img, (10,10), (150, 150), (255, 0, 0), 5)
+cv2.circle(img, (75, 75), 75, (0, 255, 0), 5)
 cv2.imshow("Holyfield vs Tyson", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
