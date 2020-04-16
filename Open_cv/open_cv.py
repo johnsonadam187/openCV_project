3633ed192251fab6c7f7dc0aa94c4622c597f2aa
Adam Johnson <johnsonadam187@gmail.com>
2020-04-16 15:39:14 +1200

    added first test imshow()

--- Open_cv/open_cv.py
+++ Open_cv/open_cv.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 import seaborn
 import cv2
 
-cap = cv2.VideoCapture("C:\Users\johns\Videos\Captures\Left hand off the cage.mp4")
+cap = cv2.VideoCapture(r"C:\Users\johns\Videos\Captures\Left hand off the cage.mp4")
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('boxing vid',frame)
+    # cv2.imshow('boxing vid',frame)
     cv2.imshow("grayscale", gray)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
