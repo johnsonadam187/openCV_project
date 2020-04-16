850ebce7b525486876503b2ba4fea575e3f33eed
Adam Johnson <johnsonadam187@gmail.com>
2020-04-16 21:48:30 +1200

    added first test imshow()

--- Open_cv/open_cv.py
+++ Open_cv/open_cv.py
@@ -7,6 +7,9 @@ import cv2
 img = cv2.imread(r"C:\Users\johns\Pictures\Saved Pictures\evanholy.jpg", cv2.IMREAD_COLOR)
 cv2.rectangle(img, (10,10), (150, 150), (255, 0, 0), 5)
 cv2.circle(img, (75, 75), 75, (0, 255, 0), 5)
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, "Holyfield", (0, 75), font, 1, (0 ,0 ,255), 2, cv2.LINE_AA)
+
 cv2.imshow("Holyfield vs Tyson", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
